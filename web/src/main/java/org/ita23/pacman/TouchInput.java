d222acc9e11f01446b0169e8ba95cc7b0eb8c26a
Lukas Knuth <me@lknuth.dev>
2025-03-08 16:09:45 +0100

    FIX: Touch input on web is now much smoother.

--- web/src/main/java/org/ita23/pacman/TouchInput.java
+++ web/src/main/java/org/ita23/pacman/TouchInput.java
@@ -8,21 +8,17 @@ import org.teavm.jso.dom.events.TouchEvent;
  * Handles Touch input (usually on mobile devices) for the pacman game.
  */
 public class TouchInput {
-    private static final double MIN_DISTANCE = 10.0;
+    private static final double MIN_DISTANCE = 45.0;
+    private static final double NOT_SET = -1.0;
   
-    private double previous_x = 0.0;
-    private double previous_y = 0.0;
-    private JoystickState state;
+    private double previous_x = NOT_SET;
+    private double previous_y = NOT_SET;
+    private JoystickState state = JoystickState.NEUTRAL;
 
     public JoystickState getCurrentState() {
       return state;
     }
   
-    public JoystickState onTouchStart(TouchEvent event) {
-      event.preventDefault();
-      return JoystickState.NEUTRAL;
-    }
-
     public JoystickState onTouchEnd(TouchEvent event) {
       event.preventDefault();
       reset();
@@ -35,12 +31,23 @@ public class TouchInput {
       return JoystickState.NEUTRAL;
     }
 
+    public JoystickState onTouchStart(TouchEvent event) {
+      event.preventDefault();
+		  Touch first_finger = event.getChangedTouches().get(0);
+		  // TODO can this even happen? if yes - share this code with move()
+		  if (first_finger != null) {
+  	    previous_x = first_finger.getClientX();
+  	    previous_y = first_finger.getClientY();
+		  }
+      return JoystickState.NEUTRAL;
+    }
+
     public JoystickState onTouchMove(TouchEvent event) {
 		  event.preventDefault();
 		  Touch first_finger = event.getChangedTouches().get(0);
 		  // TODO perhaps instead use Touch.identifier to pick a finger to track?
 		  if (first_finger != null) {
-		    double new_x = first_finger.getScreenX();
+		    double new_x = first_finger.getClientX();
 		    double new_y = first_finger.getClientY();
 		    state = handleChange(previous_x, previous_y, new_x, new_y);
 	      // Don't reset if we're waiting to hit movement threshold
@@ -76,8 +83,8 @@ public class TouchInput {
 		}
 
     private void reset() {
-	    previous_x = 0.0;
-	    previous_y = 0.0;
+	    previous_x = NOT_SET;
+	    previous_y = NOT_SET;
 	    state = JoystickState.NEUTRAL;
     }
 }
